2662ec39f730b9d0da3731ab07abe5a9a1b2dd26
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-04-25 10:12:32 +0800

    init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./**/templates/payroll_app/*.html",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
