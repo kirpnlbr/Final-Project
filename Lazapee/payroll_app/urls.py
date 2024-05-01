390084c5ca8a40e6f94a69e3a9dbd96ec13fc7fb
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-05-01 09:31:50 +0800

    feat: set up `views.py` and `urls.py`

--- Lazapee/payroll_app/urls.py
+++ Lazapee/payroll_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.employees, name='employees'),
+    path('create_employee/', views.create_employee, name='create_employee'),
+    path('payslips/', views.payslips, name='payslips'),
+    path('update_employee/', views.update_employee, name='update_employee'),
+    path('view_payslip/', views.view_payslip, name='view_payslip'),
 ]
--- Lazapee/payroll_app/views.py
+++ Lazapee/payroll_app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def employees(request):
     return render(request, 'payroll_app/employees.html')
+
+def create_employee(request):
+    return render(request, 'payroll_app/create_employee.html')
+
+def payslips(request):
+    return render(request, 'payroll_app/payslips.html')
+
+def update_employee(request):
+    return render(request, 'payroll_app/update_employee.html')
+
+def view_payslip(request):
+    return render(request, 'payroll_app/view_payslip.html')
