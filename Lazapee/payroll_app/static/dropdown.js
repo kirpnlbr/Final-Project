e96d3e519be17bc5aae221fe606e695481d36070
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-05-01 21:59:15 +0800

    fix: dropdown id number selector

--- Lazapee/payroll_app/static/dropdown.js
+++ Lazapee/payroll_app/static/dropdown.js
@@ -1,11 +1,11 @@
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById("dropdownMenu");
+function toggleDropdown(id_number) {
+    var dropdownMenu = document.getElementById("dropdownMenu-" + id_number);
     dropdownMenu.classList.toggle("hidden");
 }
 
 window.onclick = function(event) {
     if (!event.target.closest(".relative")) {
-        var dropdowns = document.querySelectorAll(".relative > .show");
+        var dropdowns = document.querySelectorAll(".absolute:not(.hidden)");
         dropdowns.forEach(function(dropdown) {
             dropdown.classList.add("hidden");
         });
