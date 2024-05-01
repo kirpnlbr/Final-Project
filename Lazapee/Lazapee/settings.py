90d36f83551038ce322dfc2982f295f5a5612ac3
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-05-01 08:12:17 +0800

    fix: tailwindcss installation
    properly this time

--- Lazapee/Lazapee/settings.py
+++ Lazapee/Lazapee/settings.py
@@ -39,6 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'payroll_app',
+    'tailwind',
+    'theme',
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'theme/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
