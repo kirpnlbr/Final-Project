5c3b989d9d5adb33a5dca122f487da3f243fd48d
Kirstine Peñalber <penalberkirstine@gmail.com>
2024-05-10 09:43:31 +0800

    chore: remove unnecessary js

--- Lazapee/payroll_app/static/dropdown.js
+++ Lazapee/payroll_app/static/dropdown.js
@@ -2,12 +2,3 @@ function toggleDropdown(id_number) {
     var dropdownMenu = document.getElementById("dropdownMenu-" + id_number);
     dropdownMenu.classList.toggle("hidden");
 }
-
-window.onclick = function(event) {
-    if (!event.target.closest(".relative")) {
-        var dropdowns = document.querySelectorAll(".absolute:not(.hidden)");
-        dropdowns.forEach(function(dropdown) {
-            dropdown.classList.add("hidden");
-        });
-    }
-}
